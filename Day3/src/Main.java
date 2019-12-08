0b8b03f8e702100016f9e401a1397a93b0f1fc4d
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2019-12-08 17:07:21 -0500

    Day3 part 2

--- Day3/src/Main.java
+++ Day3/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
 		String wire1 = "R1003,D430,L108,D570,R459,U7,L68,D232,L130,U93,R238,U951,L821,U723,L370,U873,L680,U749,R110,U17,R185,U484,R550,U356,L212,U350,L239,D208,R666,U70,L369,U448,R54,D402,R165,D375,L468,U886,L303,U779,L752,U664,L120,U643,R405,D288,L220,U727,L764,D615,R630,U688,R961,U499,L782,D852,L743,U443,R355,U856,L795,U235,L876,D511,L108,U637,R427,D338,L699,D911,L506,D607,L539,U977,R654,D634,L196,U944,R922,D774,R358,U828,L970,D386,R795,U602,R249,U793,L171,D217,L476,D123,L179,U820,R895,D239,R363,D629,L226,D811,R962,D848,R218,D581,R369,D872,L653,D281,R304,D302,R780,U636,L413,D712,L642,D886,R613,U736,L968,D82,R953,U408,L130,U654,R312,U74,L610,D798,R242,D586,L808,D664,L764,U455,R264,U384,L154,D484,R883,D276,L423,U11,L145,U156,L268,U46,R202,U641,R920,D483,R859,U94,L173,D796,R11,D328,R48,D161,L615,D396,R350,D48,R946,D233,R385,D294,R640,D301,R810,D824,L969,D469,R34,U995,R750,D827,R52,U606,R143,U868,L973,U863,L17,U995,L236,U994,R403,D312,R49,U143,L399,U821,R974,U119,R410,D233,R228,D326,R112,D512,L950,D103,L590,U80,R7,U441,L744,U643,L80,D631,L576,U680,R369,U741,L87,D748,R773,U145,R464,U546,R80,D251,L972,U414,L390,U148,L84,D481,L425,U293,L564,U880,R535,U703,R981,U944,R224,D366,R29,U517,R342,U686,L384,D650,R983,D287,L108,U713,L523,U695,R881,D126,R151,U153,R161,D791,L599,D936,L816,U387,R411,U637,L434,D22,L720,U579,L661,D644,L220,U325,R753,D392,L503,U617,R1,D956,L607,U602,L800,D749,L193,U215,L91,U733,L606,U510,L124,D550,R303,D835,R19,D326,R577,U265,L156,D924,L122,D186,R803,U3,R879";
@@ -6,7 +8,89 @@ public class Main {
 		Line[] wire1Lines = getLines(wire1);
 		Line[] wire2Lines = getLines(wire2);
 
+		System.out.println(findFewestSteps(wire1Lines, wire2Lines));
+
+	}
+
+	public static int findFewestSteps(Line[] wire1Lines, Line[] wire2Lines) {
+		Point[] intersections = getIntersections(wire1Lines, wire2Lines);
+
 		int closestDistance = -1;
+
+		for (Point p : intersections) {
+			// Find path to intersection
+			int wire1Distance = 0;
+			Point pos1 = new Point(0,0);
+
+			out:
+			for (Line l : wire1Lines) {
+				int xDiff = l.getXDiff();
+				int yDiff = l.getYDiff();
+
+				int modX = xDiff < 0 ? -1 : 1;
+				for (int d=0; d<Math.abs(xDiff); d++) {
+					pos1.x += modX;
+					wire1Distance++;
+					if (pos1.equals(p)) break out;
+				}
+
+				int modY = yDiff < 0 ? -1 : 1;
+				for (int d=0; d<Math.abs(yDiff); d++) {
+					pos1.y += modY;
+					wire1Distance++;
+					if (pos1.equals(p)) break out;
+				}
+			}
+
+			int wire2Distance = 0;
+			Point pos2 = new Point(0,0);
+
+			out2:
+			for (Line l : wire2Lines) {
+				int xDiff = l.getXDiff();
+				int yDiff = l.getYDiff();
+
+				int modX = xDiff < 0 ? -1 : 1;
+				for (int d=0; d<Math.abs(xDiff); d++) {
+					pos2.x += modX;
+					wire2Distance++;
+					if (pos2.equals(p)) break out2;
+				}
+
+				int modY = yDiff < 0 ? -1 : 1;
+				for (int d=0; d<Math.abs(yDiff); d++) {
+					pos2.y += modY;
+					wire2Distance++;
+					if (pos2.equals(p)) break out2;
+				}
+			}
+
+			int totalDistance = wire1Distance + wire2Distance;
+
+			if (closestDistance == -1 || totalDistance < closestDistance) {
+				closestDistance = totalDistance;
+			}
+		}
+
+		return closestDistance;
+	}
+
+	public static int getClosestDistance(Point[] intersections) {
+		int closestDistance = -1;
+
+		for (Point p : intersections) {
+			int distance = Math.abs(p.x) + Math.abs(p.y);
+
+			if (closestDistance == -1 || distance < closestDistance) {
+				closestDistance = distance;
+			}
+		}
+
+		return closestDistance;
+	}
+
+	public static Point[] getIntersections(Line[] wire1Lines, Line[] wire2Lines) {
+		ArrayList<Point> intersections = new ArrayList<>();
 
 		for (Line l : wire1Lines) {
 			for (Line l2 : wire2Lines) {
@@ -14,14 +98,10 @@ public class Main {
 				if (intersect == null) continue;
 				if (intersect.x == 0 && intersect.y == 0) continue;;
 
-				int distance = Math.abs(intersect.x) + Math.abs(intersect.y);
-
-				if (closestDistance == -1 || distance < closestDistance) {
-					closestDistance = distance;
-				}
+				intersections.add(intersect);
 			}
 		}
-		System.out.println(closestDistance);
+		return intersections.toArray(new Point[0]);
 	}
 	
 	public static Line[] getLines(String wire) {
@@ -71,6 +151,10 @@ class Point {
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public boolean equals(Point p) {
+		return p.x == this.x && p.y == this.y;
 	}
 
 	public String toString() {
@@ -130,6 +214,14 @@ class Line {
 		}
 
 		return new Point(endX, endY);
+	}
+
+	public int getXDiff() {
+		return this.end.x-this.start.x;
+	}
+
+	public int getYDiff() {
+		return this.end.y-this.start.y;
 	}
 
 	public String toString() {
